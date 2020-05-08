3c5a01821d6ecd644eacc9e1d79b54edabda1a77
trantbatey <trantbatey@gmail.com>
2020-05-08 00:15:20 -0500

    Completed views.

--- src/main/java/com/codeup/books/controllers/BookController.java
+++ src/main/java/com/codeup/books/controllers/BookController.java
@@ -1,24 +1,42 @@
 package com.codeup.books.controllers;
 
+import com.codeup.books.model.Book;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class BookController {
 
     @GetMapping("/books")
-    @ResponseBody
-    public String showBooks() {
-        return "Books index page";
+    public String showBooks(Model model) {
+        List<Book> bookList = new ArrayList<>();
+        Book book = new Book();
+        book.setTitle("Twilight");
+        book.setDescription("The biting story of how a high school transfer coed falls in love with a vampire.");
+        bookList.add(book);
+        book = new Book();
+        book.setTitle("The Host");
+        book.setDescription("An alien and a human fight each other for control of their body.");
+        bookList.add(book);
+
+        model.addAttribute("books", bookList);
+        return "books/index";
     }
 
     @GetMapping("/books/{id}")
-    @ResponseBody
-    public String showBook(@PathVariable int id) {
-        return "View information for an individual book " + id + ".";
+    public String showBook(@PathVariable int id, Model model) {
+        Book book = new Book();
+        book.setTitle("Twilight");
+        book.setDescription("The biting story of how a high school transfer coed falls in love with a vampire.");
+        model.addAttribute("book", book);
+        return "books/show";
     }
 
     @GetMapping("/books/create")
